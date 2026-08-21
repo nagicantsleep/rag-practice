5d881eeff85af2fab1da2bae314544dda6338d9b
nagicantsleep <54493999+nagicantsleep@users.noreply.github.com>
2026-08-22 02:45:03 +0900

    feat: export M10 production serving controls

---
+++ src/rag_practice/production/__init__.py
@@ -0,0 +1,19 @@
+from rag_practice.production.serving import (
+    GuardedServingIndex,
+    MutableLexicalIndex,
+    QueryResponse,
+    QueryTrace,
+    ServingDocument,
+    UnsafeServingIndex,
+    parse_document,
+)
+
+__all__ = [
+    "GuardedServingIndex",
+    "MutableLexicalIndex",
+    "QueryResponse",
+    "QueryTrace",
+    "ServingDocument",
+    "UnsafeServingIndex",
+    "parse_document",
+]
